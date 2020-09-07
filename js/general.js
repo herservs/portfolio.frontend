7d02df471bbcd3877418fa22e624091b8d8f9a7f
herservy <herservs@gmail.com>
2020-09-07 17:43:16 +0900

    Create JS - navbar

--- js/general.js
+++ js/general.js
@@ -1,1 +1,43 @@
 'use strict';
+
+import Navbar from '../js/navbar.js';
+
+const nav = new Navbar();
+
+document.addEventListener('scroll', () => {
+
+    const winY = window.scrollY;
+    const navY = nav.getHeight();
+
+    if (winY > navY) {
+        nav.darkNavbar();
+    } else {
+        nav.whiteNavbar();
+    }
+});
+
+
+nav.navbarMenu.addEventListener('click', (event) => {
+
+    const target = event.target;
+    const link = target.dataset.link;
+
+    if (link == null) {
+        return;
+    }
+
+    scrollIntoView(link);
+});
+
+
+
+
+
+
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({
+        behavior: 'smooth'
+    });
+};
--- js/navbar.js
+++ js/navbar.js
@@ -1,1 +1,31 @@
 'use strict';
+
+export default class Navbar {
+
+    constructor() {
+        this.navbar = document.querySelector('#navbar');
+        this.navbarLogo = document.querySelector('.navbar__logo');
+        this.navbarMenuItems = document.querySelectorAll('.navbar__menu__item');
+        this.navbarMenu = document.querySelector('.navbar__menu');
+    }
+
+    getHeight() {
+        return this.navbar.getBoundingClientRect().height;
+    }
+
+    darkNavbar() {
+        this.navbar.classList.add('navbar--dark');
+        this.navbarLogo.classList.add('navbarLogo--dark');
+        this.navbarMenuItems.forEach((navbar__menu__item) => {
+            navbar__menu__item.classList.add('navbar__menu__item--dark');
+        });
+    }
+
+    whiteNavbar() {
+        this.navbar.classList.remove('navbar--dark');
+        this.navbarLogo.classList.remove('navbarLogo--dark');
+        this.navbarMenuItems.forEach((navbar__menu__item) => {
+            navbar__menu__item.classList.remove('navbar__menu__item--dark');
+        });
+    }
+}
